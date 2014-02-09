d0dc1e1f7fa7d976684371706ed46e60e66614ca
peteratseneca <peter@Peter-MacBook-Pro-2013.local>
2014-02-09 17:21:46 -0500

    Classes project that you can use as a template

---
+++ Week_06/Classes/Classes/Launch.m
@@ -0,0 +1,32 @@
+//
+//  Launch.m
+//  Classes
+//
+//  Created by Peter McIntyre on 2/9/2014.
+//  Copyright (c) 2014 School of ICT, Seneca College. All rights reserved.
+//
+
+#import "Launch.h"
+
+@interface Launch ()
+
+@end
+
+@implementation Launch
+
+// This is just a placeholder controller
+// You can delete it whenever you're ready
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
