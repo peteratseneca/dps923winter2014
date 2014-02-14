cb556cf9b60bd8c467a6bc11100e9235da620214
peteratseneca <peter@Peter-MacBook-Pro-2013.local>
2014-02-14 10:17:01 -0500

    Project templates

---
+++ Project_Templates/ClassesV2/Classes/EntityList.h
@@ -0,0 +1,15 @@
+//
+//  EntityList.h
+//  CD Types
+//
+//  Created by Peter McIntyre on 2012/10/05.
+//  Copyright (c) 2012 Seneca College. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EntityList : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (nonatomic, strong) Model *model;
+
+@end
